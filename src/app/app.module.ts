d1defe7a95ba433eb78594d361168eba5a472e5f
Syed AzamPasha <M1027032@mindtree.com>
2019-01-03 16:07:44 +0530

    Added Product Detail Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,33 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { MatListModule } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list'; // for ul li
+import { MatGridListModule } from '@angular/material/grid-list'; // for ul li
+import { MatCardModule } from '@angular/material/card'; // for ul li
+import { MatButtonModule } from '@angular/material/button'; // for ul li
 
 import { AppComponent } from './app.component';
-
 import 'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,59 @@
 import { Product } from './../shared/product';
 import { Component, OnInit } from '@angular/core';
 
+const PRODUCTS: Product[] = [
+  {
+    id: '0',
+    name: 'shirt',
+    image: '/assets/images/products/callout-01.jpg',
+    category: 'mains',
+    featured: true,
+    label: 'mens',
+    price: 'Rs. 600',
+    description: 'Branded Shirt'
+  },
+  {
+    id: '1',
+    name: 'shirt',
+    image: '/assets/images/products/callout-02.jpg',
+    category: 'mains',
+    featured: false,
+    label: 'mens',
+    price: 'Rs. 600',
+    description: 'Branded Shirt'
+  },
+  {
+    id: '2',
+    name: 'shirt',
+    image: '/assets/images/products/callout-03.jpg',
+    category: 'mains',
+    featured: false,
+    label: 'mens',
+    price: 'Rs. 600',
+    description: 'Branded Shirt'
+  },
+  {
+    id: '3',
+    name: 'shirt',
+    image: '/assets/images/products/callout-04.jpg',
+    category: 'mains',
+    featured: false,
+    label: 'mens',
+    price: 'Rs. 600',
+    description: 'Branded Shirt'
+  },
+  {
+    id: '5',
+    name: 'shirt',
+    image: '/assets/images/products/callout-02.jpg',
+    category: 'mains',
+    featured: false,
+    label: 'mens',
+    price: 'Rs. 600',
+    description: 'Branded Shirt'
+  }
+]
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,48 +61,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  product: Product[] = [
-    {
-      id: '0',
-      name: 'shirt',
-      image: '/assets/images/products/callout-01.jpg',
-      category: 'mains',
-      featured: true,
-      label: 'mens',
-      price: 'Rs. 600',
-      description: 'Branded Shirt'
-    },
-    {
-      id: '1',
-      name: 'shirt',
-      image: '/assets/images/products/callout-02.jpg',
-      category: 'mains',
-      featured: true,
-      label: 'mens',
-      price: 'Rs. 600',
-      description: 'Branded Shirt'
-    },
-    {
-      id: '2',
-      name: 'shirt',
-      image: '/assets/images/products/callout-03.jpg',
-      category: 'mains',
-      featured: true,
-      label: 'mens',
-      price: 'Rs. 600',
-      description: 'Branded Shirt'
-    },
-    {
-      id: '3',
-      name: 'shirt',
-      image: '/assets/images/products/callout-04.jpg',
-      category: 'mains',
-      featured: true,
-      label: 'mens',
-      price: 'Rs. 600',
-      description: 'Branded Shirt'
-    }
-  ]
+  product: Product[] = PRODUCTS;
+
+  selectedProduct: Product = PRODUCTS[0];
+
   constructor() { }
 
   ngOnInit() {
